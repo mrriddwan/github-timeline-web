70d09b69510c8f85d078a1164f621818f8e46ce7
Amir Ridwan <83332805+mrriddwan@users.noreply.github.com>
2024-11-22 12:04:59 +0800

    fix(viewLayout): applied isSelected

--- src/layouts/ViewLayout.tsx
+++ src/layouts/ViewLayout.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillStar, AiFillSetting } from "react-icons/ai";
 
 function Layout({ children }: { children: React.ReactNode }) {
+  const [selectedButton, setSelectedButton] = useState<string>("Trending");
+
   return (
     <div className="bg-white min-h-screen w-full max-w-full mx-auto relative">
-      <div className="overflow-y-auto pb-20">{children}</div>
+      <div className="overflow-y-auto pb-32">{children}</div>
 
       <div className="w-full bg-slate-300 text-white p-4 flex justify-around items-center fixed bottom-0 left-0">
         <ButtonNavigate
           title="Trending"
-          icon={<AiFillStar className="text-xl" />}
-          path="/trending"
+          icon={<AiFillStar className="text-4xl" />}
+          isSelected={selectedButton === "Trending"}
+          onClick={() => setSelectedButton("Trending")}
         />
         <ButtonNavigate
           title="Setting"
-          icon={<AiFillSetting className="text-xl" />}
-          path="/search"
+          icon={<AiFillSetting className="text-4xl" />}
+          isSelected={selectedButton === "Setting"}
+          onClick={() => setSelectedButton("Setting")}
         />
       </div>
     </div>
@@ -24,22 +28,27 @@ function Layout({ children }: { children: React.ReactNode }) {
 
 export default Layout;
 
+interface ButtonNavigateProps {
+  title: string;
+  icon: React.ReactNode;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
 const ButtonNavigate = ({
   title,
   icon,
-  path,
-}: {
-  title: string;
-  icon: React.ReactNode;
-  path: string;
-}) => {
+  isSelected,
+  onClick,
+}: ButtonNavigateProps) => {
   return (
-    <button
-      className="w-full flex flex-col items-center justify-center py-2 font-semibold text-center text-gray-700"
-      onClick={() => (window.location.href = path)}
+    <div
+      className={`w-full flex flex-col items-center justify-center py-2 font-semibold text-center 
+        ${isSelected ? "text-blue-500" : "text-gray-700"}`}
+      onClick={onClick}
     >
       {icon}
       <span className="mt-1 text-xs">{title}</span>
-    </button>
+    </div>
   );
 };
